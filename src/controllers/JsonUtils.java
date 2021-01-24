e27d2c8e40f593ff09191a4135a84e4967f1f3b1
javiliebana <javierliebana99@gmail.com>
2021-01-24 13:08:36 +0100

    json utils

---
+++ src/controllers/JsonUtils.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import modelos.User;
+
+public class JsonUtils {
+	
+	static String sFile = "BBDD.json";
+
+	private static void serializarArrayAJson(ArrayList<User> lista_usuarios) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			FileWriter writer = new FileWriter(sFile);
+			gson.toJson(lista_usuarios, writer);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+
+	public static ArrayList<User> desserializarJsonAArray() {
+		ArrayList<User> usuarios = new ArrayList<User>();
+
+		try (Reader reader = new FileReader(sFile)) {
+			Gson gson = new Gson();
+			Type tipoListaEmpleados = new TypeToken<ArrayList<User>>() {
+			}.getType();
+			usuarios = gson.fromJson(reader, tipoListaEmpleados);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return usuarios;
+	}
+
+}
